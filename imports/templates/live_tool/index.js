73b05b3f932e8a8e632e0b3756f1390db4c7e3d1
Beer van der Drift <beerdrift@gmail.com>
2016-10-09 22:31:31 +0200

    Navigation back to event from livetool.

--- imports/templates/live_tool/index.js
+++ imports/templates/live_tool/index.js
@@ -2,10 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Events } from '../../collections/events.js';
 
+import { Router } from 'meteor/iron:router';
+
 import './index.html';
 
 Template.liveTool.onCreated(function() {
 	Meteor.subscribe('events');
 });
 
-Template.liveTool.helpers({ });
+Template.liveTool.events({
+	'click #backToEventBTN': function(){
+		Router.go('event', {_id: this._id});
+	}
+});

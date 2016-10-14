4fadd7b4b43084c74f29c058ddca5270442cb23e
Beer van der Drift <beerdrift@gmail.com>
2016-10-14 14:52:26 +0200

    Added create event functionality.

--- imports/templates/new_event/index.js
+++ imports/templates/new_event/index.js
@@ -1,1 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
+
+import { Event } from '../../data_structures/event.js';
+import { Events } from '../../collections/events.js';
+
 import "./index.html";
+
+Template.newEvent.events({
+	"submit #newEventForm": function(e){
+		//Don't redirect
+		e.preventDefault();
+
+		const target = e.target;
+		const eventTitle = target.etitle.value;
+
+		const eventid = Events.insert(new Event(eventTitle));
+
+		Router.go('event', {_id: eventid});
+	}
+});
